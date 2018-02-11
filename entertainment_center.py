3807df4a93b4c49229e2255594e65762f1f6e4bc
Emily <emily@fueger.net>
2018-02-11 17:05:23 -0500

    add summary and show summaries

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,7 +9,8 @@ amelie = media.Movie(
     "Amelie",
     "https://upload.wikimedia.org/wikipedia/en/5/53/Amelie_poster.jpg",
     "https://www.youtube.com/watch?v=2UT5xaAfxWU",
-    "foreign"
+    "foreign",
+    "Amelie is an innocent and naive girl in Paris with her own sense of justice. She decides to help those around her and, along the way, discovers love."
     )
 
 # Tests during creation of instances
@@ -20,14 +21,17 @@ best_in_show = media.Movie(
     "Best in Show",
     "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ5OTc0NDU1MF5BMl5BanBnXkFtZTYwNzk1OTI3._V1_.jpg",
     "https://www.youtube.com/watch?v=yeifMjqpsg0",
-    "comedy"
+    "comedy",
+    "A colorful array of characters compete at a national dog show."
     )
 
 cuckoo = media.Movie(
     "One Flew Over the Cuckoo's Nest",
     "https://images-na.ssl-images-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,672,1000_AL_.jpg",
     "https://www.youtube.com/watch?v=oUobWmLF9WQ",
-    "drama"
+    "drama",
+    "A criminal pleads insanity after getting into trouble again and once in the mental institution rebels against the oppressive nurse and rallies up the scared patients."
+
     )
 
 
@@ -35,7 +39,8 @@ forrest_gump = media.Movie(
     "Forrest Gump",
     "https://images-na.ssl-images-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
     "https://www.youtube.com/watch?v=2WSyJgydTsA",
-    "drama"
+    "drama",
+    "The presidencies of Kennedy and Johnson, Vietnam, Watergate, and other history unfold through the perspective of an Alabama man with an IQ of 75."
     )
 
 
@@ -43,7 +48,8 @@ twelve_angry_men = media.Movie(
     "12 Angry Men",
     "https://images-na.ssl-images-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SY1000_CR0,0,649,1000_AL_.jpg",
     "https://www.youtube.com/watch?v=Dosg0p7LAB4",
-    "drama"
+    "drama",
+    "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence."
     )
 
 
@@ -51,7 +57,8 @@ west_side_story = media.Movie(
     "West Side Story",
     "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM0NDAxOTI5MF5BMl5BanBnXkFtZTcwNjI4Mjg3NA@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
     "https://www.youtube.com/watch?v=NF1L3NorO3E",
-    "musical"
+    "musical",
+    "Two youngsters from rival New York City gangs fall in love, but tensions between their respective friends build toward tragedy."
     )
 
 
@@ -59,7 +66,8 @@ unforgiven = media.Movie(
     "Unforgiven",
     "https://images-na.ssl-images-amazon.com/images/M/MV5BODM3YWY4NmQtN2Y3Ni00OTg0LWFhZGQtZWE3ZWY4MTJlOWU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
     "https://www.youtube.com/watch?v=XDAXGILEdro",
-    "western"
+    "western",
+    "Retired Old West gunslinger William Munny reluctantly takes on one last job, with the help of his old partner and a young man."
     )
 
 # create array with movies from above
--- media.py
+++ media.py
@@ -6,11 +6,12 @@ import webbrowser
 
 class Movie ():
     """docstring for Movie """
-    def __init__(self, title, poster, trailer, genre):
+    def __init__(self, title, poster, trailer, genre, summary):
         self.title = title
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer
         self.genre = genre
+        self.summary = summary
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
